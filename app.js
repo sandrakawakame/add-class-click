20ac657f611e2ebf37c783eb2100cf9b5349c6f1
Sandra <sandra.kawakame@auaha.net>
2022-12-29 21:31:49 -0300

    aula 4-2 Adicionar classe ao click

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-$("[data-group]").each(function () {
+//as variaveis que estao com $ na frente é pq trazem objetos
 
-  //teste
+//selecionando todos os itens do site que tem o atributo data-group
+$("[data-group]").each(function () {
+  //seleciona todos os atributos [data-target] que estão dentro (.find) do [data-group]
   var $allTarget = $(this).find("[data-target]"),
     $allClick = $(this).find("[data-click]"),
     activeClass = "active";
 
+  //adiciona no primeiro elemento a class active
   $allTarget.first().addClass(activeClass);
   $allClick.first().addClass(activeClass);
 
+  //cria function de click
   $allClick.click(function (e) {
     e.preventDefault();
 
-    var id = $(this).data("click");
-    $target = $('[data-target="' + id + '"]');
+    //pega o valor que está dentro do data-click
+    var id = $(this).data("click"),
+      //pega o data-target que tiver
+      $target = $('[data-target="' + id + '"]');
 
     $allClick.removeClass(activeClass);
     $allTarget.removeClass(activeClass);
